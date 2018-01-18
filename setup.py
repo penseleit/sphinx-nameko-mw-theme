93228913f27c0ea64ec152a38f050d9033fe0579
Peter Enseleit <penseleit@mediaware.com.au>
2018-01-18 16:58:38 +1100

    Modify font sizing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sphinx-nameko-mw-theme',
-    version='0.0.8',
+    version='0.0.9',
     author='penseleit',
     author_email='penseleit@mediaware.com.au',
     description='Sphinx Nameko MW Theme',
